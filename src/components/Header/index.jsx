b279374b6f1632d798acde974fbdd3b3f0697c3a
Bruno Torres <br.torres.del@gmail.com>
2026-04-23 17:26:13 -0300

    Configuração da logo do header + construção do botão de menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import logoNight from "../../assets/logo/night.png";
 import logoPink from "../../assets/logo/pink.png";
 import logoSunset from "../../assets/logo/sunset.png";
 import { useTheme } from "../../hooks/useTheme";
+import { EllipsisVertical } from "lucide-react";
 
 export function Header() {
     const { theme } = useTheme();
@@ -15,12 +16,15 @@ export function Header() {
     const logoSrc = logos[theme] || logos.night;
 
     return (
-        <header className="relative z-10 top-10 left-0 w-full p-4 flex justify-center items-center">
+        <header className="relative z-10 top-10 left-0 w-full p-4 flex justify-around items-center">
             <img
                 src={logoSrc.logo}
                 alt={logoSrc.alt}
-                className="h-auto w-40 sm:w-60 lg:w-72 xl:w-80 transition duration-500 ease-in-out hover:scale-105"
+                className="h-auto w-40 sm:w-60 lg:w-72 xl:w-80 transition duration-200 ease-in-out hover:scale-110 cursor-pointer"
             />
+            <button className={`text-base-white bg-${theme}-primary/65 w-15 h-15 flex justify-center items-center rounded-full`}>
+                <EllipsisVertical />
+            </button>
         </header>
     )
 }
