3c53e12749f13da347a10c65d3a611d4381184da
Bruno Torres <br.torres.del@gmail.com>
2026-04-24 16:36:36 -0300

    Ajustes no estilo do timer para versão tablet

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -9,10 +9,10 @@ export function Timer() {
 
     return (
         <>
-            <div className="relative w-75 h-75 m-10">
-                <svg className="w-75 h-75 transform -rotate-90" viewBox="0 0 256 256">
-                    <circle cx="128" cy="128" r={radius} fill="transparent" stroke="var(--color-base-white)" strokeWidth="6" />
-                    <circle cx="128" cy="128" r={radius} fill="transparent" stroke="var(--color-night-primary)" strokeWidth="12" strokeDasharray={circunference} strokeDashoffset={circunference * 0.5} />
+            <div className="relative w-75 h-75 m-10 md:w-120 md:h-120">
+                <svg className="w-75 h-75 md:w-120 md:h-120 transform -rotate-90" viewBox="0 0 256 256">
+                    <circle cx="128" cy="125" r={radius} fill="transparent" stroke="var(--color-base-white)" strokeWidth="6" />
+                    <circle cx="128" cy="125" r={radius} fill="transparent" stroke={`var(--color-${theme}-primary)`} strokeWidth="12" strokeDasharray={circunference} strokeDashoffset={circunference * 0.5} />
                 </svg>
 
                 <div className="absolute text-center select-none left-0 top-1/2 transform -translate-y-1/2 w-full">
@@ -23,7 +23,7 @@ export function Timer() {
                 </div>
             </div>
 
-            <button className={`bg-${theme}-primary/80 hover:bg-${theme}-primary text-base-white font-body w-48 h-auto text-4xl py-2 rounded cursor-pointer`}>
+            <button className={`bg-${theme}-primary/80 hover:bg-${theme}-primary text-base-white font-body w-48 md:w-70 h-auto text-4xl md:text-6xl py-2 md:py-4 rounded cursor-pointer`}>
                 Start
             </button>
         </>
