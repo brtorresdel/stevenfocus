aa5e29f3f524c26f2d803507ba2814bd56fd1f6a
Bruno Torres <br.torres.del@gmail.com>
2026-04-24 17:16:38 -0300

    Adicionada função do timer

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,30 +1,68 @@
 import "./timer.styles.css"
 import { useTheme } from "../../hooks/useTheme"
+import { useState } from "react";
 
 export function Timer() {
     const { theme } = useTheme();
 
     const radius = 120;
     const circunference = 2 * Math.PI * radius;
+    const [isRunning, setIsRunning] = useState(false);
+    const [counter, setCounter] = useState(null);
+
+    const timers = {
+        night: {
+            title: 'Focus',
+            value: 1500
+        },
+        pink: {
+            title: 'Break',
+            value: 300
+        },
+        sunset: {
+            title: 'Break',
+            value: 900
+        }
+    }
+
+    const [currentTimer, setCurrentTimer] = useState(timers[theme].value);
+
+    const handleTimer = () => {
+        if (!isRunning) {
+            setCounter(setInterval(() => {
+                setCurrentTimer((prevTimer) => {
+                    if (prevTimer > 0) {
+                        return prevTimer - 1;
+                    }
+                    clearInterval(counter);
+                    return 0;
+                });
+            }, 1000));
+            setIsRunning(true);
+        } else {
+            clearInterval(counter);
+            setIsRunning(false);
+        }
+    };
 
     return (
         <>
             <div className="relative w-75 h-75 md:w-120 md:h-120 lg:w-75 lg:h-75 xl:w-120 xl:h-120 m-10">
                 <svg className="w-75 h-75 lg:w-75 lg:h-75 md:w-120 md:h-120 xl:w-120 xl:h-120 transform -rotate-90" viewBox="0 0 256 256">
                     <circle cx="128" cy="125" r={radius} fill="transparent" stroke="var(--color-base-white)" strokeWidth="6" />
-                    <circle cx="128" cy="125" r={radius} fill="transparent" stroke={`var(--color-${theme}-primary)`} strokeWidth="12" strokeDasharray={circunference} strokeDashoffset={circunference * 0.5} />
+                    <circle cx="128" cy="125" r={radius} fill="transparent" stroke={`var(--color-${theme}-primary)`} strokeWidth="12" strokeDasharray={circunference} strokeDashoffset={circunference * 1} />
                 </svg>
 
                 <div className="absolute text-center select-none left-0 top-1/2 transform -translate-y-1/2 w-full">
                     <h2 className="timer-title" style={{ 'color': `var(--color-${theme}-primary)`, 'text-shadow': `5px 5px 0  var(--color-${theme}-secondary)` }}>Focus</h2>
                     <span className="timer-value">
-                        25:00
+                        {new Date(currentTimer * 1000).toLocaleString('pt-BR', { minute: '2-digit', second: '2-digit' })}
                     </span>
                 </div>
             </div>
 
-            <button className={`bg-${theme}-primary/80 hover:bg-${theme}-primary text-base-white font-body w-48 lg:w-48 md:w-70 xl:w-70 h-auto text-4xl lg:text-4xl md:text-6xl xl:text-6xl  py-2 lg:py-2 md:py-4 xl:py-4 rounded cursor-pointer`}>
-                Start
+            <button className={`bg-${theme}-primary/80 hover:bg-${theme}-primary text-base-white font-body w-48 lg:w-48 md:w-70 xl:w-70 h-auto text-4xl lg:text-4xl md:text-6xl xl:text-6xl  py-2 lg:py-2 md:py-4 xl:py-4 rounded cursor-pointer`} onClick={handleTimer}>
+                {isRunning ? 'Pausar' : 'Iniciar'}
             </button>
         </>
     )
