7a7f723f87a1d526183fda64bc149e2121693d51
Bruno Torres <br.torres.del@gmail.com>
2026-04-27 12:52:33 -0300

    Ajuste no CSS do header da lista de tarefas

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,6 +1,12 @@
-export function ToDoList () {
+import "./todolist.styles.css";
+
+export function ToDoList() {
     return (
-        <>
-        </>
+        <div className="mt-5 sm:mt-10">
+            <div className="flex flex-row justify-between items-center w-full">
+                <h2 className="todo-header font-title text-base-white text-lg sm:text-2xl">Lista de Tarefas</h2>
+                <button className="todo-header font-title text-base-white text-4xl hover:scale-150 duration-200 ease-in-out cursor-pointer">+</button>
+            </div>
+        </div>
     )
 }
