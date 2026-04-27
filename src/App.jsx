3a0a32fe8912fb202899625c2b4dec98ff971e58
Bruno Torres <br.torres.del@gmail.com>
2026-04-27 12:22:43 -0300

    Início da construção da lista de tarefas + linha divisória entre o timer e a lista de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Background } from './components/Background'
 import { Header } from './components/Header'
 import { Timer } from './components/Timer'
+import { ToDoList } from './components/ToDoList'
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
       <Header />
       <div>
         <Timer />
+        <hr className='mt-10 text-base-white border-dashed w-auto text-center border-2 lg:hidden'/>
+        <ToDoList />
       </div>
     </Background>
   )
