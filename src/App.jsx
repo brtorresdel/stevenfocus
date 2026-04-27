3e7f2f06181f7fc6b8c6c1bda656c30a810ffc06
Bruno Torres <br.torres.del@gmail.com>
2026-04-27 13:00:36 -0300

    Ajuste na exibição da lista de tarefas na versão desktop

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Background>
       <Header />
-      <div className='w-4/5'>
+      <div className='w-4/5 flex flex-col lg:w-full lg:justify-center lg:flex-row-reverse'>
         <Timer />
         <hr className='mt-10 text-base-white border-dashed w-auto text-center border-2 lg:hidden'/>
         <ToDoList />
--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -2,9 +2,9 @@ import "./todolist.styles.css";
 
 export function ToDoList() {
     return (
-        <div className="mt-5 sm:mt-10">
+        <div className="mt-5 sm:mt-10 w-full lg:w-2/5">
             <div className="flex flex-row justify-between items-center w-full">
-                <h2 className="todo-header font-title text-base-white text-lg sm:text-2xl">Lista de Tarefas</h2>
+                <h2 className="todo-header font-title text-base-white text-lg sm:text-3xl lg:text-2xl xl:text-3xl">Lista de Tarefas</h2>
                 <button className="todo-header font-title text-base-white text-4xl hover:scale-150 duration-200 ease-in-out cursor-pointer">+</button>
             </div>
         </div>
