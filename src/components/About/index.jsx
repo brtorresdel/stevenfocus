f71441493fb96c4f821baeba980915de1b3463cc
Bruno Torres <br.torres.del@gmail.com>
2026-04-29 18:37:12 -0300

    Construção da sessão about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,7 +1,34 @@
+import about from "../../data/about.json";
+
+const socialMediaIcons = {}
+
 export function About() {
     return (
-        <div>
-            <h2>Dev: Bruno Torres</h2>
+        <div className="flex flex-col gap-5">
+            <div>
+                <div className="flex flex-row items-end gap-2">
+                    <h2 className="text-xl">Dev:</h2>
+                    <p className="text-2xl">{about.dev.name}</p>
+                </div>
+                <div>
+                    {about.dev.socialMedia.map(soc => {
+                        return <a href={soc.link}>{soc.name}</a>
+                    })}
+                </div>
+            </div>
+
+            <div>
+                <div className="flex flex-row items-end gap-2">
+                    <h2 className="text-xl">Design / Arts:</h2>
+                    <p className="text-2xl">{about.design.name}</p>
+                </div>
+                <div>
+                    {about.design.socialMedia.map(soc => {
+                        return <a href={soc.link}>{soc.name}</a>
+                    })}
+                </div>
+            </div>
+
         </div>
     )
 }
