b682abb63ca84be2fcdd2f34aaf46dd999b006fb
Bruno Torres <br.torres.del@gmail.com>
2026-04-30 14:24:58 -0300

    Ajuste do componente de tarefa para marcação de conclusão

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,20 +1,26 @@
 import { useModal } from "../../hooks/useModal";
 import { useTheme } from "../../hooks/useTheme";
 import { Check, CheckCheck, Pen } from "lucide-react";
+import { useTasks } from './../../hooks/useTasks/index';
 
 export function Task({ taskInfo }) {
 
     const { theme, themeConfig } = useTheme();
     const { openTaskDescriptionView } = useModal();
+    const { handleCompletedTask } = useTasks();
 
     return (
-        <button className={`flex flex-row justify-between items-center p-4 w-full h-10 md:h-13 ${themeConfig[theme].colors.btn} ${themeConfig[theme].colors.btnHover} border-base-white border rounded-lg text-base-white font-body-light ${taskInfo.isCompleted ? "line-through" : ""} mb-3 cursor-pointer`}
-            onClick={() => openTaskDescriptionView(taskInfo)}>
-            <div className="flex flex-row gap-2">
-                <Check className={`hover:scale-140 transition duration-200 ${taskInfo.isCompleted ? "hidden" : ""}`} />
-                <CheckCheck className={`hover:scale-140 transition duration-200 ${taskInfo.isCompleted ? "" : "hidden"}`} />
-                <p className="sm:text-xl">{taskInfo.name}</p>
+
+        <>
+            <div className={`flex flex-row justify-start items-center gap-2 p-4 w-full h-10 md:h-13 ${themeConfig[theme].colors.btn} ${themeConfig[theme].colors.btnHover} border-base-white border rounded-lg text-base-white font-body-light ${taskInfo.isCompleted ? "line-through" : ""} mb-3`}>
+                <button onClick={() => handleCompletedTask(taskInfo.id)} className="cursor-pointer">
+                    <Check className={`hover:scale-140 transition duration-200 ${taskInfo.isCompleted ? "hidden" : ""}`} />
+                    <CheckCheck className={`hover:scale-140 transition duration-200 ${taskInfo.isCompleted ? "" : "hidden"}`} />
+                </button>
+                <div onClick={() => openTaskDescriptionView(taskInfo)} className="grow">
+                    <p className="sm:text-xl hover:underline cursor-pointer" >{taskInfo.name}</p>
+                </div>
             </div>
-        </button>
+        </>
     )
 }
